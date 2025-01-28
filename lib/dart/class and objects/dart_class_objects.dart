173b867c32f4cecd1841227d16b834e14033a851
Hasti <hastivasoya7609@gmail.com>
2025-01-28 18:27:57 +0530

    new class and objects and functions

--- lib/dart/class and objects/dart_class_objects.dart
+++ lib/dart/class and objects/dart_class_objects.dart
@@ -1,3 +1,14 @@
+
 void main(){
   print("Creating and Objects");
+  Human().numbers();
+}
+
+class Human{
+ void numbers(){
+   int num = 50;
+   int num2 = 10;
+   int num3 = num + num2;
+    print(num3);
+ }
 }
