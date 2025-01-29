a3cf6016172b33c3fa33e60603a25dcf5f35b82b
Hasti <hastivasoya7609@gmail.com>
2025-01-29 13:50:04 +0530

    added 2 Text field

--- lib/flutter widgets/form.dart
+++ lib/flutter widgets/form.dart
@@ -17,6 +17,55 @@ class _FormScreenState extends State<FormScreen> {
         style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.deepPurpleAccent,
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 18.0,horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: Text("Student Login Form",
+              style: Theme.of(context).textTheme.headlineMedium,),
+            ),
+            SizedBox(height: 20,),
+            /// first name
+            Text("First Name",
+            style: Theme.of(context).textTheme.titleLarge,
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.purple,
+                  )
+                ),
+                labelText: "Enter Your First Name"
+              ),
+            ),
+            /// last name
+            SizedBox(height: 20,),
+            Text("Last Name",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      )
+                  ),
+                  labelText: "Enter Your Last Name",
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
