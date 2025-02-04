eff6e55952574894dea098e6488b3546beb65624
Hasti <hastivasoya7609@gmail.com>
2025-02-04 11:21:08 +0530

    adding navigations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:strating_flutter/flutter%20widgets/provider/list_page.dart';
+import 'package:strating_flutter/navigations/pages/main_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ListPageScreen(),
+      home: const MainPagesScreen(),
     );
   }
 }
